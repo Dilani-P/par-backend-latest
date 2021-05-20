92e96c14c8e223fbca2660cd3b320c9fd7e29941
Dilani-P <dilani@veracity.dev>
2021-05-20 15:33:12 +0530

    create interface file and define attributes of users

---
+++ src/Routes/Users/types.ts
@@ -0,0 +1,10 @@
+// attributes of the users
+export interface IUser {
+    firstname: string ,
+    lastname: string,
+    role: string,
+    password : string,
+    invitedAt : string,
+    invitedBy : string,
+    acceptedAt : string
+}
